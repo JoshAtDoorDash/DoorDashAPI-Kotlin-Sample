6813b12244a34ba851886c11a376ea8ca43b1b21
Josh Hurley <109998658+JoshAtDoorDash@users.noreply.github.com>
2023-02-23 15:42:51 -0500

    Added Source

---
+++ source/DoorDashAPIKotlinSample/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ source/DoorDashAPIKotlinSample/src/main/kotlin/Main.kt
@@ -0,0 +1,68 @@
+import io.jsonwebtoken.Header
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.security.Key
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.*
+import kotlin.collections.HashMap
+
+fun main(args: Array<String>) {
+    val claims = HashMap<String, Any?>()
+
+    // Populate claims used that will be used in JWT Payload
+    claims["aud"] = "doordash"
+    claims["iss"] = "UPDATE_WITH_DEVELOPER_ID" // TODO: Update value with Developer ID
+    claims["kid"] = "UPDATE_WITH_KEY_ID" // TODO: Update value with Key ID
+    claims["exp"] = ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(30).toEpochSecond()
+    claims["iat"] = ZonedDateTime.now(ZoneOffset.UTC).toEpochSecond()
+
+    // Signing Secret is Base64Encoded when generated on the Credentials page, need to decode to use
+    val keyBytes = Decoders.BASE64URL.decode("UPDATE_WITH_SIGNING_SECRET") // TODO: Update value with Signing Secret
+    val key: Key = Keys.hmacShaKeyFor(keyBytes)
+
+    // Create JWT
+    val jwt: String = Jwts.builder()
+        .setHeaderParam("dd-ver", "DD-JWT-V1")
+        .setHeaderParam("typ", "JWT")
+        .setClaims(claims)
+        .signWith(key)
+        .compact()
+
+    // Write the DoorDash API JWT
+    println("DoorDash API JWT: $jwt")
+
+    // Generate Unique ID for Delivery
+    val deliveryId = UUID.randomUUID(); // TODO: Replace with generated system ID
+
+    // Create data needed to create a new delivery
+    val body = """{
+    "external_delivery_id": "$deliveryId",
+    "pickup_address": "901 Market Street 6th Floor San Francisco, CA 94103",
+    "pickup_business_name": "Wells Fargo SF Downtown",
+    "pickup_phone_number": "+16505555555",
+    "pickup_instructions": "Enter gate code 1234 on the callbox.",
+    "dropoff_address": "901 Market Street 6th Floor San Francisco, CA 94103",
+    "dropoff_business_name": "Wells Fargo SF Downtown",
+    "dropoff_phone_number": "+16505555555",
+    "dropoff_instructions": "Enter gate code 1234 on the callbox.",
+    "order_value": 1999
+    }"""
+
+    val client = HttpClient.newBuilder().build();
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://openapi.doordash.com/drive/v2/deliveries/"))
+        .header("Authorization", "Bearer $jwt")
+        .header("Content-Type", "application/json")
+        .POST(HttpRequest.BodyPublishers.ofString(body))
+        .build();
+
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    // Write response body for create new delivery call
+    println(response.body())
+}
